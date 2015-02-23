5986085bdbc09cdddafd5ca3a3f6bf1d0ac98a63
jerome@work <github@jeromedecoster.com>
2015-02-23 15:43:31 +0100

    tape ie9 fail

--- test/index.js
+++ test/index.js
@@ -5,6 +5,10 @@ test('simple comparisons', function (t) {
 	t.plan(1);
 
 	var n = maxBy([9,3,4], function (x) { return x % 3 });
-  console.log('ua:', navigator.userAgent)
-	t.equal(n, 4);
+  // force fail on ie9
+  if (navigator.userAgent.indexOf('MSIE 9.0') >-1) {
+    t.equal(n, 5);
+  } else {
+	  t.equal(n, 4);
+  }
 });
