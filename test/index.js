7a91c722006979fd63a4879453930c2ed9d7cb3b
jerome@work <github@jeromedecoster.com>
2015-06-03 11:52:28 +0200

    tape ie9..latest

--- test/index.js
+++ test/index.js
@@ -4,11 +4,15 @@ var test = require('tape');
 test('simple comparisons', function (t) {
 	t.plan(1);
 
+  console.log(navigator.userAgent + ' // ' + navigator.platform)
 	var n = maxBy([9,3,4], function (x) { return x % 3 });
   // force fail on ie9
-  if (navigator.userAgent.indexOf('MSIE 9.0') >-1) {
-    t.equal(n, 5);
-  } else {
+  // if (navigator.userAgent.indexOf('MSIE 9.0') >-1) {
+  //   t.equal(n, 5);
+  // force fail on chrome
+  // } else if (navigator.userAgent.indexOf('Chrome') >-1) {
+  //   t.equal(n, 5);
+  // } else {
 	  t.equal(n, 4);
-  }
+  // }
 });
